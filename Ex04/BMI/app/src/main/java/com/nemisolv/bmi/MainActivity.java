45628c204fe02c033f7622ec57182991d9a23cc6
Ducanh230703 <2151173746@e.tlu.edu.vn.>
2024-10-14 10:15:16 +0700

    ex4 mainac

--- Ex04/BMI/app/src/main/java/com/nemisolv/bmi/MainActivity.java
+++ Ex04/BMI/app/src/main/java/com/nemisolv/bmi/MainActivity.java
@@ -37,7 +37,30 @@ public class MainActivity extends AppCompatActivity {
         editWeight = findViewById(R.id.editTextWeight);
         editDiagnosis = findViewById(R.id.editTextDiagnosis);
         editBMI = findViewById(R.id.editTextBMI);
+        btnBmi.setOnClickListener(v -> {
+            String name = editName.getText().toString();
+            String height = editHeight.getText().toString();
+            String weight = editWeight.getText().toString();
+            double h = Double.parseDouble(height);
+            double w = Double.parseDouble(weight);
+            double bmi = w / (h * h);
+            String result = "";
+            if (bmi < 18) {
+                result = "Người gầy";
+            } else if (bmi >= 18.5 && bmi < 24.9) {
+                result = "Người bình thường";
+            } else if (bmi >= 25 && bmi < 29.9) {
+                result = "Người béo phì cấp 1";
+            } else if(bmi >=30 && bmi < 34.9) {
+                result = "Người béo phì cấp 2";
+            }
 
+            else {
+                result = "Người béo phì cấp 3";
+            }
+            DecimalFormat df = new DecimalFormat("0.#");
+            editBMI.setText(df.format(bmi));
+            editDiagnosis.setText(result);
         });
 
 
