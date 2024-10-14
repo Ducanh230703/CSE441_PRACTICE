fa2dc2b294708a2357e6eaab47b1340c798a6eaa
Ducanh230703 <2151173746@e.tlu.edu.vn.>
2024-10-14 22:47:23 +0700

    loi

--- Ex11/intent-filter-call-website/app/src/main/java/com/nemisolv/intent_filter/MainActivity.java
+++ Ex11/intent-filter-call-website/app/src/main/java/com/nemisolv/intent_filter/MainActivity.java
@@ -22,19 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btnSearch = findViewById(R.id.btn_search);
-        txtSearch = findViewById(R.id.edit_text_search);
-        btnSearch.setOnClickListener(v -> {
-            String search = txtSearch.getText().toString();
-            if (!search.isEmpty()) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("https:%s", search)));
 
-                startActivity(intent);
-            }else {
-                Toast.makeText(this, "Please enter a search query", Toast.LENGTH_SHORT).show();
-                return;
-            }
         });
 
     }
