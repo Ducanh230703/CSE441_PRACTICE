d1740a950a6df4a9e7ecd2de81431834eaa177bd
Ducanh230703 <2151173746@e.tlu.edu.vn.>
2024-10-16 16:11:31 +0700

    fix main

--- Ex13-CustomLayout/b1-phone-list/app/src/main/java/com/nemisolv/phonelist/MainActivity.java
+++ Ex13-CustomLayout/b1-phone-list/app/src/main/java/com/nemisolv/phonelist/MainActivity.java
@@ -28,7 +28,16 @@ public class MainActivity extends AppCompatActivity {
     private void initial() {
         lvPhones = findViewById(R.id.listView);
         phoneList = List.of(
-                ent, view, position, id) -> {
+                new Phone("Iphone 15 Pro Max",R.drawable.image1),
+                new Phone("Iphone 12 Pro Max",R.drawable.image2),
+                new Phone("Samsung s22 ultra",R.drawable.image3),
+                new Phone("HTC 1", R.drawable.image4),
+                new Phone("Xiaomi mi 8",R.drawable.image5)
+                );
+        myArrayAdapter = new MyArrayAdapter(this,R.layout.list_view_item,phoneList);
+        lvPhones.setAdapter(myArrayAdapter);
+
+        lvPhones.setOnItemClickListener((parent, view, position, id) -> {
             Intent intent = new Intent(this,ResultActivity.class);
             Phone current = myArrayAdapter.getItem(position);
             intent.putExtra("name",current.getName());
