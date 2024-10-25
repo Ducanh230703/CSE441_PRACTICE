a4562a4b4cf0b4fa073ed4cf1d9cbb63b30a22d8
Ducanh230703 <2151173746@e.tlu.edu.vn.>
2024-10-25 12:03:51 +0700

    adapter fix

--- Ex24_final/app/src/main/java/net/nemisolv/stock_exchange/ExchangeRateAdapter.java
+++ Ex24_final/app/src/main/java/net/nemisolv/stock_exchange/ExchangeRateAdapter.java
@@ -22,29 +22,5 @@ public class ExchangeRateAdapter extends ArrayAdapter<ExchangeRate> {
         this.exchangeRates = exchangeRates;
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
-        ExchangeRate exchangeRate = getItem(position);
 
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
-        }
-
-        ImageView imgThumbnail = convertView.findViewById(R.id.img_thumbnail);
-        TextView textCurrencyType = convertView.findViewById(R.id.text_currency_type);
-        TextView textBuyCash = convertView.findViewById(R.id.text_buy_cash);
-        TextView textSellCash = convertView.findViewById(R.id.text_sell_cash);
-
-        textCurrencyType.setText(exchangeRate.getCurrencyType());
-        textBuyCash.setText("Buy Cash: " + exchangeRate.getBuyCash());
-        textSellCash.setText("Sell Cash: " + exchangeRate.getSellCash());
-
-        Bitmap thumbnail = exchangeRate.getThumbnail();
-        if (thumbnail != null) {
-            imgThumbnail.setImageBitmap(thumbnail);
-        }
-
-        return convertView;
-    }
 }
