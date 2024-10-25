df4ab28757e54c4426c57233a95c1a5e49bb98e5
Ducanh230703 <2151173746@e.tlu.edu.vn.>
2024-10-25 12:02:10 +0700

    read json

--- Ex21/app/src/main/java/net/nemisolv/read_json/MainActivity.java
+++ Ex21/app/src/main/java/net/nemisolv/read_json/MainActivity.java
@@ -43,27 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private List<String> readJSON() {
-        try {
-            InputStream is = getAssets().open("computer.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            String json = new String(buffer, "UTF-8");
-            JSONObject obj = new JSONObject(json);
-            List<String> list = new ArrayList<>();
-            list.add(obj.getString("category_id"));
-            list.add(obj.getString("category_name"));
-            for (int i = 0; i < obj.getJSONArray("products").length(); i++) {
-                JSONObject computer = obj.getJSONArray("products").getJSONObject(i);
-                list.add(computer.getString("id"));
-                list.add(computer.getString("name"));
-                list.add(computer.getString("price"));
-            }
-            return list;
-        } catch (IOException | JSONException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
